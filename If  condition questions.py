ccea4d0dff1c58a05e9cff3798a9610eacc273a0
Sakshi Rawat <sakshirwt557@gmail.com>
2026-03-20 22:18:47 +0530

    Add files via upload

---
+++ If  condition questions.py
@@ -0,0 +1,62 @@
+Q1. Write a Python program to check if a number is positive.
+
+num=int(input("Enter a number:"))
+if num >0:
+    print("number is positive")
+
+
+Q2.Print "Eligible to vote" if age is 18 or above
+age=int(input(" Enter a age :"))
+
+if age>=18:
+ print("Eligible to vote")
+
+Q3.Check if a number is divisible by 7.
+
+num=int(input("Enter a number:"))
+if num/7:
+    print("number is divisible by 7")
+    
+Q4.Print "Pass" if marks are greater than 40.
+
+marks =int(input("Enter a number:"))
+if marks>40:
+    print("pass")
+    
+Q5.Check if a number is greater than 100.
+
+
+num=int(input("Enter a number:"))
+if num>100:
+    print("number is greater than 100")
+
+Q6.Display a message if temperature exceeds 45°C.
+
+temp=float(input("Enter a temp:"))
+if temp>45:
+  print("temp exceeds 45c")
+  
+Q7.Check if a string length is more than 8 characters.
+
+text=input("Enter a string:")
+if len(text)>8:
+    print("String length is more than 8 character")
+
+Q8.Check if a number is a multiple of 10.
+
+num=int(input("Enter a number:"))
+if num%10==0:
+    print("number is multiple of 10")
+    
+Q9.Print "Logged In" if password matches "admin123".
+
+password=int(input("Enter a password:"))
+if password=="admin123":
+    print("logged in")
+  
+Q10.Print a warning if balance is below minimum limit.
+
+  balance=float(input("Enter balance:"))
+if balance<1000:
+  print("Warning:Balance below minimum limit")
+
