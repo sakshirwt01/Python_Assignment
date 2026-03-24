726abe7fafcc70e4e956e2f8ee9fa02026fe2827
Sakshi Rawat <sakshirwt557@gmail.com>
2026-03-24 22:25:57 +0530

    Add files via upload

---
+++ Elif Questions.py
@@ -0,0 +1,117 @@
+"""
+Q1. Print day name using day number (1–7).
+
+day=int(input("Enter day number(1-7):"))
+if day==1:
+    print("Monday")
+elif day==2:
+    print("Tuesday")
+elif day==3:
+    print("Wednesday")
+elif day==4:
+    print("Thursday")
+elif day==5:
+    print("Friday")
+elif day==6:
+    print("Saturday")
+elif day==7:
+    print("Sunday")
+else:
+    print("Invalid date")
+    
+Q2.Print month name using month number.
+
+month=int(input("Enter month num:"))
+if month==1:
+  print("january")
+elif month==2:
+  print("february")
+elif month==3:
+  print("march")
+elif month==4:
+  print("april")
+
+Q3. Display grade based on percentage.
+
+per=int(input("Enter percentage:"))
+if per>=90:
+  print("Grade A")
+elif per>=75:
+  print("Grade B")
+elif per>=50:
+  print("Grade c")
+else:
+  print("fail")
+  
+Q4.Display bonus percentage based on experience years.
+
+exp=int(input("Enter experience:"))
+if exp>=10:
+    print("Bonus 20%")
+elif exp>=5:
+    print("Bonus 10%")
+else:
+    print("Bonus 5%")
+
+Q5.Identify traffic signal meaning.
+
+signal=input("Enter color name:")
+if signal=="red":
+    print("stop")
+elif signal=="yellow":
+    print("wait")
+elif signal=="green":
+    print("go")
+    
+Q6.Categorize temperature as Cold / Warm / Hot.
+
+temp=int(input("Enter temp:"))
+if temp<20:
+    print("cold")
+elif temp<=30:
+    print("warm")
+else:
+    print("hot")
+    
+Q7.Categorize employee based on salary range.
+
+salary=int(input("Enter salary:"))
+if salary>=80000:
+    print("high level employee")
+elif salary>=30000:
+    print("mid level employee")
+else:
+    print("junior employee")
+
+Q8.Print discount percentage based on purchase amount.
+
+amount=int(input("Enter amount:"))
+if amount>=5000:
+    print("20% discount")
+elif amount>=2000:
+    print("10% discount")
+else:
+    print("no discount")
+
+Q9.Identify number type: single-digit / double-digit / multi-digit.
+
+num=int(input("Enter number:"))
+if num<10:
+    print("single diggit")
+elif num<100:
+    print("double digit")
+else:
+    print("multi digit")
+
+Q10.Assign performance rating: Poor / Average / Good / Excellent.
+
+score=int(input("Enter score:"))
+if score>=90:
+    print("excellent")
+elif score>=70:
+    print("good")
+elif score>=50:
+    print("average")
+else:
+    print("poor")
+"""
