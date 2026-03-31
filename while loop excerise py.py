d62d3eda35c013a292f2b875b72e461a2b74618f
Sakshi Rawat <sakshirwt557@gmail.com>
2026-03-31 13:05:41 +0530

    Update while loop excerise py.py

--- while loop excerise py.py
+++ while loop excerise py.py
@@ -211,16 +211,7 @@ while i <= 50:
 
 
 """
-i = 2
-while i <= 50:
-    j = 2
-    is_prime = True
 
-    while j < i:
-        if i % j == 0:
-            is_prime = False
-            break
-        j = j + 1
     if is_prime:
         print(i)
     i = i + 1
